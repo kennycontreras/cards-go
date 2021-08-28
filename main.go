cde50a246858efb36fe4755393e64991b61ad918
Eugenio Contreras <eugeniocontreras.globant@latam.com>
2021-08-28 18:49:02 -0400

    Adding new functions to save and load files

--- main.go
+++ main.go
@@ -1,16 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
 
-	cards := newDeck()
-
+	cards := newDeckFromFile("my_cards_test.txt")
 	cards.print()
-	fmt.Println("here ")
-
-	hand, remainingCards := deal(cards, 5)
-
-	hand.print()
-	remainingCards.print()
 }
